020ccdd70461ba90d38d138fafc4aa76053651fe
bvieira <bvieira@magnasistemas.com.br>
2025-02-17 20:33:39 -0300

    feat: Plot graphics of probability and execution time

--- kSat.py
+++ kSat.py
@@ -1,15 +1,14 @@
 import random
 from pysat.solvers import Solver
+import matplotlib.pyplot as plt
+import numpy as np
 
 def gerar_instancia_sat(n, m, k):
     variaveis = list(range(1, n + 1))
     clausulas = []
 
     while len(clausulas) < m:
-        # Seleciona k variáveis distintas
         vars_clausula = random.sample(variaveis, k)
-        
-        # Decide aleatoriamente se cada variável será positiva ou negada
         clausula = []
         for var in vars_clausula:
             if random.choice([True, False]):
@@ -17,28 +16,42 @@ def gerar_instancia_sat(n, m, k):
             else:
                 clausula.append(-var)
         
-        # Verifica se a cláusula já existe
+        # Verificae se a clausula ja existe
         if clausula not in clausulas:
             clausulas.append(clausula)
     
     return clausulas
 
 def verificar_satisfazibilidade(clausulas):
-    solver = Solver(name='glucose3')  # Usando o solver Glucose
+    solver = Solver(name='glucose3')  
     for clausula in clausulas:
-        solver.add_clause(clausula)  # Adiciona cada cláusula ao solver
-    satisfazivel = solver.solve()  # Verifica se é satisfazível
-    solver.delete()  # Libera a memória do solver
+        solver.add_clause(clausula)
+    satisfazivel = solver.solve() 
+    solver.delete() 
     return satisfazivel
 
 def calcular_probabilidade_satisfazibilidade(n, alpha, k, num_instancias=30):
-    m = int(alpha * n)  # Número de cláusulas
+    m = int(alpha * n)
     satisfaziveis = 0
-
     for _ in range(num_instancias):
         instancia = gerar_instancia_sat(n, m, k)
         if verificar_satisfazibilidade(instancia):
             satisfaziveis += 1
     
     probabilidade = satisfaziveis / num_instancias
+   
     return probabilidade
+
+def gerar_grafico_probabilidade(n, k, list_p, valores_alpha):
+    fig, ax = plt.subplots()
+    plt.title(f"Grafico de probabilidade_{k}-SAT_N={n}")
+    ax.plot(valores_alpha, list_p)
+    fig.savefig(f"IMAGES/PROBABILIDADE/{k}-SAT_N={n}")
+    plt.show()
+
+def gerar_grafico_tempo(n, k, valores_alpha, t):
+    fig, ax = plt.subplots()
+    plt.title(f"Grafico de tempo_{k}-SAT_N={n}")
+    ax.plot(valores_alpha, t)
+    fig.savefig(f"IMAGES/TEMPO/{k}-SAT_N={n}")
+    plt.show()
--- main.py
+++ main.py
@@ -1,20 +1,38 @@
 import time
-from kSat import calcular_probabilidade_satisfazibilidade
-import pysat
+from kSat import calcular_probabilidade_satisfazibilidade, gerar_grafico_probabilidade, gerar_grafico_tempo
+import numpy as np
 
-def main():
-    valores_n = [50, 100, 150, 200]  # Valores de n
-    valores_alpha = [3.0, 3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 3.7, 3.8, 3.9, 4.0, 4.1, 4.2, 4.3, 4.4, 4.5, 4.6, 4.7, 4.8, 4.9, 5.0]  # Valores de alpha
-    k = 3  # Tamanho das cláusulas (3-SAT)
-
+def testar_sat(valores_n, valores_alpha, k, nome_sat):
+    
+    print(f"Testando {nome_sat} (k = {k})")
     for n in valores_n:
+        list_probabilidade = []
+        list_tempo = []
         print(f"n = {n}")
         for alpha in valores_alpha:
             inicio = time.time()
             probabilidade = calcular_probabilidade_satisfazibilidade(n, alpha, k)
             fim = time.time()
-            print(f"  alpha = {alpha}: Probabilidade = {probabilidade:.4f}, Tempo = {fim - inicio:.2f} segundos")
+            tempo = round(float(fim - inicio), 2)
+            print(f"  alpha = {alpha}: Probabilidade = {probabilidade:.4f}, Tempo = {tempo} segundos")
+            list_probabilidade.append(probabilidade)
+            list_tempo.append(tempo)
+        if list_probabilidade != None:
+            gerar_grafico_probabilidade(n, k, list_probabilidade, valores_alpha)
+            gerar_grafico_tempo(n, k, valores_alpha, list_tempo)
         print()
+
+def main():
+    valores_n = [50, 100, 150, 200]  # Valores de n
+    valores_alpha_3sat = [round(valor, 1) for valor in np.arange(3.0, 5.1, 0.1)] # de 3 a 5 incremendantpo 0.1
+    valores_alpha_5sat = [round(valor, 1) for valor in np.arange(1, 20.1, 0.1)]# de 1 a 20 incremendanto 0.1
+
+    # Testar 3-SAT
+    testar_sat(valores_n, valores_alpha_3sat, k=3, nome_sat="3-SAT")
+    
+
+    # Testar 5-SAT
+    testar_sat(valores_n, valores_alpha_5sat, k=5, nome_sat="5-SAT")
 
 if __name__ == "__main__":
     main()
