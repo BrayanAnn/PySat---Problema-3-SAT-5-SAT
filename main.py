6baa26c0bb8ba6aebd7c7e208c90ca2135ac0877
bvieira <bvieira@magnasistemas.com.br>
2025-02-20 14:17:00 -0300

    feat: Change n values to 5sat test

--- main.py
+++ main.py
@@ -23,16 +23,17 @@ def testar_sat(valores_n, valores_alpha, k, nome_sat):
         print()
 
 def main():
-    valores_n = [50, 100, 150, 200]  # Valores de n
+    valores_n_3sat = [50, 100, 150, 200]  # Valores de n
+    valores_n_5sat = [10, 20, 30, 40, 50, 60]
     valores_alpha_3sat = [round(valor, 1) for valor in np.arange(3.0, 5.1, 0.1)] # de 3 a 5 incremendantpo 0.1
     valores_alpha_5sat = [round(valor, 1) for valor in np.arange(1, 20.1, 0.1)]# de 1 a 20 incremendanto 0.1
 
     # Testar 3-SAT
-    testar_sat(valores_n, valores_alpha_3sat, k=3, nome_sat="3-SAT")
+    testar_sat(valores_n_3sat, valores_alpha_3sat, k=3, nome_sat="3-SAT")
     
 
     # Testar 5-SAT
-    testar_sat(valores_n, valores_alpha_5sat, k=5, nome_sat="5-SAT")
+    testar_sat(valores_n_5sat, valores_alpha_5sat, k=5, nome_sat="5-SAT")
 
 if __name__ == "__main__":
     main()
